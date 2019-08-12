b836dd40a48beb1555cc8fddf53dfff9e47c1964
Lucas <lucas@alternativainternet.com>
2019-08-12 13:40:32 -0300

    user authentication

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/user', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddlewares);
 
 export default routes;
