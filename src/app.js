a64c3fb77ddb80a28907c1883d31828f35d5ef4a
Lucas <lucas@alternativainternet.com>
2019-08-25 01:51:02 -0300

    added cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -16,6 +17,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
